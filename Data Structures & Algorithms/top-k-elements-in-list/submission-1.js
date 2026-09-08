f49c08739e028eca4aeec35380cd48291950a95c
Mahesh Nanera <84780820+maheshnanera@users.noreply.github.com>
2026-09-08 16:33:13 +0530

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+
+        let lastSeen = new Map()
+
+        for(let i=0 ; i <nums.length ; i++){
+            const num = nums[i]
+
+            lastSeen.set(num, (lastSeen.get(num) || 0) + 1);
+        } 
+
+        const sorted = [...lastSeen.entries()].sort((a,b) => b[1] - a[1])    
+
+    return sorted.slice(0,k).map(item => item[0]);
+
+    }
+}
